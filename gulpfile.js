7bb2f12b7d2cfc3709801090e6440a0f6dbc2446
santosh sagar <msg2santoshsagar@gmail.com>
2017-04-27 08:15:32 +0530

    Added script to rename files using gulp-rev.

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ var uglify = require('gulp-uglify');
 var clean = require('gulp-clean');
 var runSequence = require('run-sequence');
 var inject = require('./gulp/inject');
+var rename = require('gulp-rename');
+var print = require('gulp-print');
+var rev   = require('gulp-rev');
 
 gulp.task('minify', function() {
 	  gulp.src(['WebContent/app/**/*.js'])
@@ -22,9 +25,25 @@ gulp.task('minify', function() {
 
 gulp.task('inject:dev', inject.app);
 
+gulp.task('deleteMinFiles',function(){
+	return gulp.src(['WebContent/app/**/*min.js'])
+	.pipe(clean({force:true}))
+});
+
+gulp.task('addTimeStamp',function(){
+	return gulp.src(['WebContent/app/**/*.js','!WebContent/app/**/*min.js'])
+		.pipe(print())
+		.pipe(rev())
+		.pipe(rename(function (path) {
+  			path.basename += ".min";
+	}))
+.pipe(gulp.dest("WebContent/app/"));
+});
 
 
-
+gulp.task('rename',function(){
+	runSequence('deleteMinFiles','addTimeStamp');
+});
 
 gulp.task('default', ['minify'],function(cb){
 	
